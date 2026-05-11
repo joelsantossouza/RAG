19cab5980370ddbf5dd8a4da7fc4a4c15f853c48
Joel Santos Souza <joesanto@c2r6s7.42porto.com>
2026-05-11 16:10:37 +0100

    retrieving chunks

--- student/__main__.py
+++ student/__main__.py
@@ -4,11 +4,12 @@ def main() -> None:
     """
     """
     index = Indexing()
-    index.load_files("/home/joesanto/Downloads/10kb.txt")
+    index.load_files("data/vllm-0.10.1/")
     index.build_chunks()
-    for chunk in index.chunks:
-        print(chunk)
-        print()
+    index.build_indexes()
+    for chunk in index.retrieve("What activation formats does the fused batched MoE layer return in vLLM?", 1):
+        print(chunk.file_path)
+        print(index._read_chunk(chunk))
 
 if __name__ == "__main__":
     main()
--- student/indexing/indexing.py
+++ student/indexing/indexing.py
@@ -2,7 +2,9 @@ from pathlib import Path, PosixPath
 from ..models import MinimalSource
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-
+from bm25s import BM25
+from bm25s.tokenization import Tokenized as BM25Tokenized
+import bm25s
 
 
 class Indexing:
@@ -12,6 +14,7 @@ class Indexing:
     def __init__(self) -> None:
         self.files_path: list[PosixPath] = []
         self.chunks: list[MinimalSource] = []
+        self.bm25: BM25 = BM25()
 
     def load_files(self, path: str) -> None:
         root: Path = Path(path)
@@ -28,11 +31,21 @@ class Indexing:
                 continue
             self.files_path.append(file_path)
 
+    def _read_chunk(self, chunk: MinimalSource) -> str:
+        with open(chunk.file_path,
+                  "r",
+                  encoding="utf-8",
+                  errors="ignore") as f:
+            f.seek(chunk.first_character_index)
+            return f.read(
+                chunk.last_character_index - chunk.first_character_index
+            )
+
     def build_chunks(self, chunk_size: int = 2000) -> None:
         splitter = RecursiveCharacterTextSplitter(
             chunk_size=chunk_size,
             chunk_overlap=0,
-            keep_separator=False,
+            keep_separator=True,
             add_start_index=True,
         )
 
@@ -53,3 +66,18 @@ class Indexing:
                         last_character_index=end,
                     )
                 )
+
+    def build_indexes(self) -> None:
+        corpus: list[str] = [
+            self._read_chunk(chunk) for chunk in self.chunks
+        ]
+        tokenized_corpus: BM25Tokenized = bm25s.tokenize(corpus)
+        self.bm25.index(tokenized_corpus)
+
+    def retrieve(self, query: str, top_k: int = 5) -> list[MinimalSource]:
+        tokenized_query: BM25Tokenized = bm25s.tokenize(query)
+        indices, _ = self.bm25.retrieve(
+            tokenized_query,
+            k=top_k,
+        )
+        return [self.chunks[i] for i in indices[0]]
